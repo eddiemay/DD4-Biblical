d07c263441a8c3b2095792286c82be1569a71230
eddiemay <eddiemay1999@yahoo.com>
2024-06-17 15:38:04 -0700

    langchain chat exercises.

--- src/main/ml/langchain_chat_gradio.py
+++ src/main/ml/langchain_chat_gradio.py
@@ -1,6 +1,5 @@
 import gradio
 from pprint import pprint
-from langchain.chains import ConversationalRetrievalChain
 from langchain.chains import RetrievalQA
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
@@ -10,76 +9,39 @@ from langchain_openai import OpenAIEmbeddings
 
 llm_name = "gpt-4o"
 CHROMA_PATH = "chatbot/chroma/"
-
-embedding = OpenAIEmbeddings()
-vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding)
-
-question = "How many children did Abraham have?"
-docs = vectordb.similarity_search(question,k=3)
-pprint(docs)
-
-llm = ChatOpenAI(model_name=llm_name, temperature=0)
-llm.invoke("Hello world!")
-
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Always say "thanks for asking!" at the end of the answer. 
 {context}
 Question: {question}
 Helpful Answer:"""
-QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
+QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template)
 
+vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=OpenAIEmbeddings())
 memory = ConversationBufferMemory(
     memory_key="chat_history",
+    output_key="result",
     return_messages=True)
 
 qa_chain = RetrievalQA.from_chain_type(
-    llm,
+    ChatOpenAI(model_name=llm_name, temperature=0),
     retriever=vectordb.as_retriever(),
     memory=memory,
+    return_source_documents=True,
     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
 
 
-result = qa_chain({"query": question})
-print(result["result"])
-
-memory = ConversationBufferMemory(
-    memory_key="chat_history",
-    return_messages=True)
-
-retriever=vectordb.as_retriever()
-qa = ConversationalRetrievalChain.from_llm(
-    llm,
-    retriever=retriever,
-    memory=memory
-)
-
-result = qa({"question": question})
-pprint(result['answer'])
-
-result = qa({"question": "What are the names of his children?"})
-pprint(result['answer'])
-
-question = "How many wives did he have?"
-result = qa({"question": question})
-pprint(result['answer'])
-
-
 # chat interface
-def chat_function_qa_chain(question, history):
+def chat_function(question, history):
     response = qa_chain({"query": question})
-    return response["result"]
+    return '{}\n\nReferences:{}'.format(
+        response["result"],
+        "".join("\n{} {}".format(doc.metadata['reference'], doc.page_content) for doc in response["source_documents"]))
 
 
-# chat interface
-def chat_function_llm(question, history):
-    response = qa({"question": question})
-    return response["answer"]
-
+pprint(chat_function("How long did Jacob serve for Rachel?", ""))
 
 # Set up the Gradio chat interface
 iface = gradio.ChatInterface(
-    fn=chat_function_qa_chain,
-    title="Intelligent Search Assistant",
+    fn=chat_function,
+    title="Bible Search Assistant",
     description="This interface uses the bible to answer your questions.",
-    theme="default")
-
-iface.launch(share=False)
+    theme="default").launch(share=True)
