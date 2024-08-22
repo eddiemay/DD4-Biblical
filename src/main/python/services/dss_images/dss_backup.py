5e1ed11df745a391c0154b341696ee2eb56e44c0
eddiemay <eddiemay1999@yahoo.com>
2024-08-22 13:53:47 -0700

    Making use of scroll json rather than calculating.

--- src/main/python/services/dss_images/dss_backup.py
+++ src/main/python/services/dss_images/dss_backup.py
@@ -1,30 +1,54 @@
+import json
 import os
+import numpy as np
 import requests
-from constants import Collection
 from multiprocessing import Pool
 from pathlib import Path
 
-FILE_PATH = 'images/{}/{}_{}_{}.jpg'
-DSS_URL = 'http://tiles.imj.org.il/tiles/{}/{}_{}_{}.jpg'
-MB_URL = 'https://imgprd21.museumofthebible.org/collections/Tiled/35a9f923-d461-46f2-a946-98380a355606/TileGroup{}/{}-{}-{}.jpg'
+FILE_DIR = 'images/{}/'
+FILE_NAME = '{}_{}_{}.jpg'
+DSS_JSON_URL = 'http://dss.collections.imj.org.il/viewer/data/{}.json'
+MB_TILE_URL = 'https://imgprd21.museumofthebible.org/collections/Tiled/35a9f923-d461-46f2-a946-98380a355606/TileGroup{}/{}-{}-{}.jpg'
 
 
-def download(scroll, res, col, row):
+def get_json(scroll):
+    dir = FILE_DIR.format(scroll)
+    Path(dir).mkdir(parents=True, exist_ok=True)
+    file_path = dir + scroll + '.json'
+    if os.path.isfile(file_path) and os.path.getsize(file_path) > 1024:
+        with open(file_path, 'r') as f:
+            ret = json.load(f)
+            ret['scroll'] = scroll
+            return ret
+
+    response = requests.get(DSS_JSON_URL.format(scroll))
+    with open(file_path, 'w') as output:
+        output.write(response.text)
+    ret = json.load(response.text)
+    ret['scroll'] = scroll
+    return ret
+
+
+def download(collection, col, row, group=0):
+    scroll = collection.scroll
+    res = collection.res
     Path('images/' + scroll).mkdir(parents=True, exist_ok=True)
-    file_path = FILE_PATH.format(scroll, res, col, row)
+    file_name = FILE_NAME.format(res, col, row)
+    file_path = FILE_DIR.format(scroll) + file_name
     if os.path.isfile(file_path) and os.path.getsize(file_path) > 1024:
         # print('File {} exists, exiting'.format(file_path))
         return
+
     if scroll == 'torah':
-        if row < 3 or row == 3 and col < 12:
-            group = 0
-        elif row < 13 or row == 13 and col < 18:
-            group = 1
-        else:
-            group = 2
-        url = MB_URL.format(group, res, col, row)
+        # if row < 3 or row == 3 and col < 12:
+          #  group = 0
+        # elif row < 13 or row == 13 and col < 18:
+          #  group = 1
+        # else:
+           # group = 2
+        url = MB_TILE_URL.format(group, res, col, row)
     else:
-        url = DSS_URL.format(scroll, res, col, row)
+        url = collection.tile_prefix + file_name
     print('Downloading {} to {}'.format(url, file_path))
     response = requests.get(url)
     resp_len = len(response.content)
@@ -32,31 +56,97 @@ def download(scroll, res, col, row):
     if resp_len > 1024:
         with open(file_path, 'wb') as output:
             output.write(response.content)
+    elif scroll == 'torah':
+        download(collection, col, row, group + 1)
+
+
+class Collection:
+    def __init__(self, scroll=None, res=None, cols=None, rows=None,
+      tile_size=None, column_offsets=None, tile_prefix=None, json=None):
+        self.scroll = scroll
+        self.res = res
+        self.tile_prefix = tile_prefix
+
+        if json is not None:
+            if scroll is None:
+                self.scroll = json['scroll']
+            if tile_prefix is None:
+                self.tile_prefix = json['tilePrefix']
+            height = int(json['height'])
+            width = int(json['width'])
+            ratio = int(json['fullSize']['w']) / width
+            max_zoom = int(json['maxZoom'])
+            if res is None:
+                self.res = res = max_zoom - 1
+            for x in range(res, max_zoom):
+                ratio /= 2
+
+            self.tile_size = json['tileSize']
+            self.rows = round(height * ratio / self.tile_size)
+            self.cols = round(width * ratio / self.tile_size)
+            columns = json['columns']
+            cs = len(columns)
+            self.column_offsets = [None] * (cs + 1)
+            for c in range(cs):
+                column = columns[c]
+                self.column_offsets[int(column['id'])] = column['x'] * ratio
+            self.column_offsets[0] = (
+              self.column_offsets[1] + columns[cs - 1]['width'] * ratio)
+            self.column_offsets = np.array(self.column_offsets)
+        else:
+            self.tile_size = tile_size
+            self.rows = rows
+            self.cols = cols
+            self.column_offsets = column_offsets
+
+    def to_string(self):
+        return (
+            'scroll: {}, res: {}, tile_size: {}, rows: {}, cols: {}, '
+            'tile_prefix: {}, column_offsets: {}').format(
+            self.scroll, self.res, self.tile_size, self.rows, self.cols,
+            self.tile_prefix, self.column_offsets)
+
+
+Collection.ISAIAH = Collection(json=get_json('isaiah'))
+Collection.WAR = Collection(json=get_json('war'))
+Collection.COMMUNITY_RULE = Collection(json=get_json('community'))
+Collection.TEMPLE_SCROLL = Collection(res=9, json=get_json('temple'))
+Collection.HABAKKUK = Collection(json=get_json('habakkuk'))
+Collection.TORAH_5 =(
+    Collection('torah', 5, 24, 17, 256, [5500, 4400, 3300, 2200, 1100, 0]))
+Collection.TORAH_4 =(
+    Collection('torah', 4, 14, 9, 256, [2750, 2200, 1650, 1100, 550, 0]))
 
 
 class Tile:
-    def __init__(self, scroll, res, col, row):
-        self.scroll = scroll
-        self.res = res
+    def __init__(self, collection, col, row):
+        self.collection = collection
         self.col = col
         self.row = row
 
 
 def download_tile(tile):
-    download(tile.scroll, tile.res, tile.col, tile.row)
+    download(tile.collection, tile.col, tile.row)
 
 
 def download_collection(collection):
     tiles = []
-    for col in range(collection.cols + 1):
-        for row in range(collection.rows + 1):
-            tiles.append(Tile(collection.scroll, collection.res, col, row))
+    for col in range(collection.cols):
+        for row in range(collection.rows):
+            tiles.append(Tile(collection, col, row))
 
     with Pool() as pool:
         pool.map(download_tile, tiles)
 
 
 if __name__ == '__main__':
+    print(Collection.ISAIAH.to_string())
+    print(Collection.WAR.to_string())
+    print(Collection.COMMUNITY_RULE.to_string())
+    print(Collection.HABAKKUK.to_string())
+    print(Collection.TEMPLE_SCROLL.to_string())
+    print(Collection.TORAH.to_string())
+
     # Great Isaiah Scroll
     download_collection(Collection.ISAIAH)
 
--- src/main/python/services/dss_images/stitch.py
+++ src/main/python/services/dss_images/stitch.py
@@ -1,35 +1,32 @@
 import cv2
 import math
 import numpy
-from constants import Collection
-from dss_backup import download
+from dss_backup import Collection, download, get_json
 from dss_ocr import image_to_string
 
 BASE_PATH = r'images/{}/{}_{}_{}.jpg'
 
 
-def load_img(scroll, res, col, row):
-    download(scroll, res, col, row)
-    file_path = BASE_PATH.format(scroll, res, col, row)
+def load_img(collection, col, row):
+    download(collection, col, row)
+    file_path = BASE_PATH.format(collection.scroll, collection.res, col, row)
     return cv2.imread(file_path)
 
 
 def construct_column(collection, column=1):
     column_offsets = collection.column_offsets
-    print(
-        'bounds: {}'.format(
-            [column_offsets[column], column_offsets[column - 1]]))
+    print('bounds: {}'.format(
+      [column_offsets[column], column_offsets[column - 1]]))
     rows = collection.rows
     tile_size = collection.tile_size
     col_offset = math.floor(column_offsets[column] / tile_size)
     cols = math.ceil(column_offsets[column - 1] / tile_size) - col_offset
-    print(
-        'Creating canvas of size: {}'.format(
-            [rows * tile_size, cols * tile_size]))
+    print('Creating canvas of size: (w: {}, h: {})'.format(
+        cols * tile_size, rows * tile_size))
     canvas = numpy.zeros((rows * tile_size, cols * tile_size, 3), dtype='uint8')
     for c in range(cols):
         for r in range(rows):
-            img = load_img(collection.scroll, collection.res, c + col_offset, r)
+            img = load_img(collection, c + col_offset, r)
             x = c * tile_size
             y = r * tile_size
             canvas[y:y + img.shape[0], x:x + img.shape[1]] = img
@@ -37,14 +34,34 @@ def construct_column(collection, column=1):
     print('Showing image...')
     cv2.imshow('Canvas', canvas)
     print(image_to_string(canvas, False))
-    cv2.imwrite(
-        'images/{}/column{}.jpg'.format(collection.scroll, column), canvas)
+    # cv2.imwrite('images/{}/column{}.jpg'.format(collection.scroll, column), canvas)
     cv2.waitKey(0)
 
+
+construct_column(Collection.TORAH_5, 5)
+construct_column(Collection.TORAH_4, 5)
+construct_column(Collection.TORAH_5, 4)
+construct_column(Collection.TORAH_4, 4)
+construct_column(Collection.TORAH_5, 1)
+construct_column(Collection.TORAH_4, 1)
+
+construct_column(Collection.ISAIAH, 54)
+construct_column(Collection.ISAIAH, 53)
+construct_column(Collection.ISAIAH, 1)
 
 construct_column(Collection.WAR, 15)
 construct_column(Collection.WAR, 14)
 construct_column(Collection.WAR, 1)
-construct_column(Collection.TORAH, 1)
-# for c in range(1, 55):
-construct_column(Collection.ISAIAH, 1)
+
+construct_column(Collection.COMMUNITY_RULE, 11)
+construct_column(Collection.COMMUNITY_RULE, 10)
+construct_column(Collection.COMMUNITY_RULE, 1)
+
+construct_column(Collection.TEMPLE_SCROLL, 67)
+construct_column(Collection.TEMPLE_SCROLL, 66)
+construct_column(Collection.TEMPLE_SCROLL, 2)
+construct_column(Collection.TEMPLE_SCROLL, 1)
+
+construct_column(Collection.HABAKKUK, 14)
+construct_column(Collection.HABAKKUK, 13)
+construct_column(Collection.HABAKKUK, 1)
