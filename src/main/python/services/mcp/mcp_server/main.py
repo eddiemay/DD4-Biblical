ebe3775b6a327eb57f2d2f5c6a1e5f1ae86ebd67
eddiemay <eddiemay1999@yahoo.com>
2025-07-10 10:54:30 -0700

    Very minor code cleanup

--- src/main/python/services/mcp/mcp_server/main.py
+++ src/main/python/services/mcp/mcp_server/main.py
@@ -1,12 +1,10 @@
 import asyncio
 from mcp_server import mcp
 
-
 if __name__ == "__main__":
-  asyncio.run(
-      mcp.run_async(
-          transport="sse",
-          host="0.0.0.0",
-          port=8080,
-          path="/mcp",
-          log_level="debug"))
+  asyncio.run(mcp.run_async(
+      transport="sse",
+      host="0.0.0.0",
+      port=8080,
+      path="/mcp",
+      log_level="debug"))
